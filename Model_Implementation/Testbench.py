4a8a79395afd738fc448f32cd20d09fcf68e3346
mm6396 <mhr.alizade.com>
2024-04-25 23:41:13 -0400

    Testbench

--- Model_Implementation/Testbench.py
+++ Model_Implementation/Testbench.py
@@ -6,61 +6,129 @@
 # Basically, once this testbench is up and running, we can tweak/re-tweak DRLA using the training set to optimize performance.
 # NOTE: NOT a good idea to tweak the classifier, only the AL method!!!
 
-import sys, os
+# INITIAL_SAMPLES = 130
+BATCH_SIZE = 32
+N_S =1 # one sample each time from unlabeled data
+# EPOCHS = 5
+# LEARNING_RATE = 0.001
+# NUM_CLASSES = 2
+# INPUT_SHAPE = (224, 224, 3)
+#regarding the Jeremy's Comment. I will modify it 
+
 
 import numpy as np
-from matplotlib import pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras.models import Model
+import h5py
 
-from ActiveLearningMethods import *
+from ActiveLearningMethods import EntropyStrategy, RandomSamplingStrategy , LeastConfidenceStrategy
+def convert_to_one_hot(labels, num_classes):
+    
+    one_hot_labels = tf.keras.utils.to_categorical(labels, num_classes=num_classes)
+    return one_hot_labels.squeeze()  
+
+def resize_images(images):
+    ###Resize images to the standard for ResNet50.
+    return tf.image.resize(images, [224, 224])
+
+class ResNet50Classifier:
+    def __init__(self, input_shape, num_class, lr=0.001):
+        self.input_shape = input_shape
+        self.num_class = num_class
+        self.lr = lr
+        self.model = self._build_model()
+
+    def _build_model(self):
+        base_model = ResNet50(weights='imagenet', include_top=False, input_shape=self.input_shape)
+        x = GlobalAveragePooling2D()(base_model.output)
+        predictions = Dense(self.num_class, activation='softmax')(x)
+        model = Model(inputs=base_model.input, outputs=predictions)
+        model.compile(optimizer=Adam(learning_rate=self.lr), loss='categorical_crossentropy', metrics=['accuracy'])
+        return model
+    
+    def predict(self, x):
+        return self.model.predict(x)
+
+def load_data(path):
+    with h5py.File(path + 'train250_train_x.h5', 'r') as file:
+        x_train = np.array(file['x'][:])
+    with h5py.File(path + 'train250_train_y.h5', 'r') as file:
+        y_train = np.array(file['y'][:])
+        
+        
+    with h5py.File(path + 'train250_val_x.h5', 'r') as file:
+        x_val = np.array(file['x'][:])
+    with h5py.File(path + 'train250_val_y.h5', 'r') as file:
+        y_val = np.array(file['y'][:])
+    return x_train, y_train , x_val , y_val
 
 
-def train_on_AL_approach(image_classifier: object, al_instance: ActiveLearningMethod, Dataset_params: object, other_params: object):
-    """
-    Given an Active Learning Method and a Dataset, train our image_classifier according to the ActiveLearning strategy.
-    """
-    # prepare starting labels (Follow DRLA paper for comparison on 2 of our datasets, we'll decide ourselves on cancer MNIST.
-
-    # Start epoch loop:
-
-    # Classifier predicts on all data (labeled or unlabeled) (State)
-
-    # call al_instance.choose_n_samples() to decide which samples to annotate.
-
-    # Update classifier weights (1 backprop iteration) for 1 epoch on all of the labeled samples.
-
-    # Classifier predicts again on all data (labeled or unlabeled) (New_State)
-
-    # Call al_instance.update_on_new_state(). This will either do nothing for most methods, or update Actor/Critic for DRLA.
-
-    # End epoch loop.........
-
-    # Could also be a tuple if we want.
-    someDataStructureContainingManyMetrics = {"Stuff": 123}
-
-    return someDataStructureContainingManyMetrics
 
 def main():
+    print("Starting the main function.\n")
+    
+    path = '/Users/mehrnoushalizade/Desktop/TA-solutions/ActiveLearningProject/PatchCamelyon/output/'
 
-    # This script should be hardcoded into Train mode most of the time, but can be configured to do the Test dataset at the end.
+    x_train, y_train, x_val, y_val = load_data(path)
+    x_train = resize_images(x_train)  
+    y_train = convert_to_one_hot(y_train, num_classes=2)
 
-    # Choose/prepare dataset (Chose 1 of the 3. Or I guess we could go through all 3)
-    # ASSUME REPOSITORY DIRECTORY STRUCTURE (and data is available!)
-    # We can port this to Turing or some other compute service if needed then by just cloning git repository!
+    x_val = resize_images(x_val)
+    y_val = convert_to_one_hot(y_val, num_classes=2)
 
-    # Instantiate Resnet50 model
+    strategies = [EntropyStrategy(), RandomSamplingStrategy(), LeastConfidenceStrategy()  
+                    #DRLA
+                    ]
 
-    # Instantiate class for each of the Active Learning approaches we'll be comparing
+    for strategy in strategies:
+        print(f"------------------- << {strategy} >> --------------------------")
+        classifier =  ResNet50Classifier(input_shape=(224, 224, 3), num_class=2) 
+        model = classifier
 
-    # Loop: For each AL approach, call train_on_AL_approach(parameters). Collect metrics from this run.
+        num_samples = len(x_train)
+        initial_samples = 130  
 
-    # Build table - follow intervals like in DRLA paper
+        
+        labeled_mask = np.zeros(num_samples, dtype=bool)
+        initial_indices = np.random.choice(num_samples, initial_samples, replace=False)
+        labeled_mask[initial_indices] = True
 
-    # Build per-epoch chart - do accuracy, sensitivity, specificity
+        # Initial labeled data
+        x_labeled = np.array(x_train)[initial_indices]
+        y_labeled = np.array(y_train)[initial_indices]
 
-    # Save charts, data for this set of runs.
+        # Unlabeled data indices
+        remaining_indices = np.where(labeled_mask==False)[0]
 
-    pass
+        while len(remaining_indices) > 0:
+            print(f"The Strategy is: {strategy} \n")
 
+            # Choose one new samples from unlabeled pool to label it
+            predictions = model.predict(np.array(x_train)[remaining_indices])  # Predict on unlabeled data
+            print(f"dimension labeled_mask is : {labeled_mask.shape}")
+            
+            selected_indices = strategy.choose_n_samples(N_S, predictions, labeled_mask)
+            labeled_mask[remaining_indices[selected_indices]] = True
+
+            # Update label mask
+            labeled_indices = np.where(labeled_mask==True)[0]
+            x_labeled = np.array(x_train)[labeled_indices]
+            y_labeled = np.array(y_train)[labeled_indices]
+
+            # Train the classifier with all labeled data
+            model.fit(x_labeled, y_labeled, epochs=1, validation_data=(x_val, y_val), batch_size=BATCH_SIZE)
+
+            # Evaluate the classifier (new_perfomance)
+            new_performance = model.evaluate(x_val, y_val)
+            print(f"new performance: {new_performance}")
+
+            # Update the remaining indices
+            remaining_indices = np.where(labeled_mask==False)[0]
+
+        print(f"Active learning with {strategy} completed.")
 
 if __name__ == "__main__":
     main()
